973a27faa8e99c514e0bd2313a2d53742c635057
Ysandys Perez <ysandysp@gmail.com>
2021-09-09 17:58:16 -0300

    mi primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import validator from './validator.js';
+//Tomar datos de la tarjeta escrita en input
+document.getElementById("myBut").addEventListener("click", function() {
+  let cardNumber= document.getElementById("blankspace").value;
+  document.getElementById("firstnumber").innerHTML= validator.maskify(cardNumber);
+  //Llamar a las unciones isValid y maskify
+  validator.isValid(cardNumber);
+  validator.maskify(cardNumber);
 
-console.log(validator);
+
+  console.log(validator.isValid(cardNumber), validator.maskify(cardNumber));
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,47 @@
 const validator = {
-  // ...
+  isValid(cardNumber){
+    let arrayNum= cardNumber.split(""); //convertir mi string en array
+    let reverseNum= arrayNum.reverse(); //poner el número en reversa
+//crear variable para suma y booleana.
+    let suma= 0;
+    //multiplicar las posiciones pares
+    for(let i = 0; i<reverseNum.length; i++){
+      if(i % 2 === 1){
+        let multNum= reverseNum[i]*2;
+        if(multNum >= 10){
+          multNum= multNum-9;
+          suma+= multNum;
+        }else{
+          suma+= multNum;
+        }
+        }
+      else{
+        suma+= parseInt(reverseNum[i]);
+        }
+      }
+    if(suma % 10 === 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  },
+  maskify(cardNumber){
+
+    const invNum= cardNumber.split("").reverse();
+    let secretNum= "";
+     for(let i = 0; i<invNum.length; i++){
+       if(i < 4){
+          secretNum+= invNum[i];
+       }
+       else{
+         secretNum+= "#";
+       }
+      }
+    const maskNum= secretNum.split("").reverse().join("");
+    return maskNum;
+     }
+
 };
 
 export default validator;
